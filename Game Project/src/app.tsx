0dbda9e14b0dec2cf56119aed5c2d3e355bb60c3
M.Ali Dadashi <122849217+Malidadashi@users.noreply.github.com>
2024-02-08 20:39:32 +0330

    first commit

---
+++ Game Project/src/app.tsx
@@ -0,0 +1,8 @@
+import ListGroup from "./Components/ListGroup"
+const App = () => {
+  return (
+    <div>ListGroup</div>
+  )
+}
+
+export default App
---
+++ Game Project/src/main.tsx
@@ -0,0 +1,5 @@
+import { render } from 'preact'
+import { App } from './app.tsx'
+import './index.css'
+import 'bootstrap/dist/css/bootstrap.css'
+render(<App />, document.getElementById('app')!)
